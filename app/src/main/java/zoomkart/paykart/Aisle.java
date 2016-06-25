f0c69f2445e4823b745f1408ca2e46f8148e6e3c
Sudhanva Huruli <sshuruli@uwaterloo.ca>
2016-06-25 17:04:41 -0400

    NFC Activity skeleton

---
+++ app/src/main/java/zoomkart/paykart/Aisle.java
@@ -0,0 +1,14 @@
+package zoomkart.paykart;
+
+/**
+ * Created by sudhanvahuruli on 6/25/16.
+ */
+public class Aisle {
+    public int aisleNumber;
+    public int deviceId;
+
+    public Aisle(int aisleNumber, int deviceId){
+        this.aisleNumber = aisleNumber;
+        this.deviceId = deviceId;
+    }
+}
--- app/src/main/java/zoomkart/paykart/NFCPairActivity.java
+++ app/src/main/java/zoomkart/paykart/NFCPairActivity.java
@@ -1,13 +1,96 @@
 package zoomkart.paykart;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NFCPairActivity extends AppCompatActivity {
+import java.nio.charset.Charset;
+
+public class NFCPairActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
+
+    NfcAdapter mNfcAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcpair);
+
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+
+        NdefRecord uriRecord = new NdefRecord(
+                NdefRecord.TNF_ABSOLUTE_URI ,
+                "http://zoomkart".getBytes(Charset.forName("US-ASCII")),
+                new byte[0], new byte[0]);
+
+        return new NdefMessage(uriRecord);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+
+            if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+                Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+                if (rawMsgs != null) {
+                    NdefMessage[] msgs = new NdefMessage[rawMsgs.length];
+
+                    for (int i = 0; i < rawMsgs.length; i++) {
+                        msgs[i] = (NdefMessage) rawMsgs[i];
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        Toast.makeText(this, new String(msg.getRecords()[0].getPayload()), Toast.LENGTH_LONG).show();
+
+        // Make an API call to the server here by generating a unique device id and the aisle number
+        // Now the server knows the phone and aisle pairing
+        // When the items are scanned, the raspberry pie will send a bulk message with the
+        // aisle number and all the items to the server
+        // Server will do a lookup of prices and make a call back to the phone when the phone
+        // wants to pay the bill 
+
     }
 }
