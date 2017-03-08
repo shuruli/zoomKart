b145713d978bbd82b709b73b308367151b831434
Sudhanva Huruli <sshuruli@uwaterloo.ca>
2017-03-08 18:53:17 -0500

    fix login activity

--- app/src/main/java/zoomkart/paykart/models/Item.java
+++ app/src/main/java/zoomkart/paykart/models/Item.java
@@ -5,10 +5,6 @@ package zoomkart.paykart.models;
  * Created by sudhanvahuruli on 6/25/16.
  */
 public class Item {
-    public int mId;
-    public String mName;
-    public double mPrice;
-    public int mQuantity;
-    public String mCategory;
-    public String mImage;
+    public String name;
+    public double price;
 }
--- app/src/main/java/zoomkart/paykart/models/ListItems.java
+++ app/src/main/java/zoomkart/paykart/models/ListItems.java
@@ -1,8 +1,19 @@
 package zoomkart.paykart.models;
 
+import com.google.gson.Gson;
+
+import java.util.List;
+
 /**
  * Created by sudhanvahuruli on 7/25/16.
  */
 public class ListItems {
-    public Item[] items;
+    public List<Item> listItems;
+
+    public static ListItems fromJson(String s) {
+        return new Gson().fromJson(s, ListItems.class);
+    }
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 }
